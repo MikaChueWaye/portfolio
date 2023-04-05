a13ceac52b4adfa75b2b72a4f9d5b864767a5a74
Mika ChueWaye <mika-lock-yane.chue-waye@etu.umontpellier.fr>
2023-04-05 23:21:47 +0200

    Ajout de l'effet "Hack" sur tous les éléments de la classe "hacked" + ajout de la normalisation des éléments de base du site (sur le css)

--- js/hackEffect.js
+++ js/hackEffect.js
@@ -3,34 +3,31 @@ const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let interval = null;
 let word;
 
-document.querySelector(".hacked").onmouseover = event => {
-    let iteration = 0;
+let elements = document.querySelectorAll('.hacked');
+elements.forEach(event => event.addEventListener('mouseover', () => {
+        let iteration = 0;
 
-    // word = event.target.innerText;
+        clearInterval(interval);
 
-    clearInterval(interval);
+        interval = setInterval(()=> {
+            event.innerHTML = event.innerHTML
+                .split("")
+                .map((letter, index) => {
+                    if (index < iteration)
+                    {
+                        return event.dataset.value[index];
+                    }
+                    return letters[Math.floor(Math.random()*26)]
 
-    interval = setInterval(()=> {
-        event.target.innerText = event.target.innerText
-            .split("")
-            .map((letter, index) => {
-                if (index < iteration)
-                {
-                    return event.target.dataset.value[index];
-                    // return word.charAt(index);
-                }
-                return letters[Math.floor(Math.random()*26)]
+                })
+                .join("");
 
-            })
-            .join("");
+            if (iteration >= event.dataset.value.length)
+            {
+                clearInterval(interval);
+            }
 
-        if (iteration >= event.target.dataset.value.length)
-            // if (iteration >= word.length)
-        {
-            clearInterval(interval);
-        }
-
-        iteration += 1 / 3;
-    }, 30);
-}
-
+            iteration += 1 / 3;
+        }, 30);
+    })
+)
