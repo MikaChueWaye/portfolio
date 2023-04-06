a41d940a6a32c58c4b2f17631da645e120ac7645
Chue-Waye Mikalockyane <mika-lock-yane.chue-waye@etu.umontpellier.fr>
2023-04-06 13:58:24 +0200

    ajout de texte sur l'index html ajout d'un effet hack personnel au contenu du site

--- js/hackEffect.js
+++ js/hackEffect.js
@@ -31,4 +31,35 @@ elements.forEach(event => event.addEventListener('mouseover', () => {
             iteration += 1 / 3;
         }, 30);
     })
+);
+
+let elements2 = document.querySelectorAll('.FastHacked');
+elements2.forEach(event => event.addEventListener('mouseover', () => {
+        let interval = null;
+
+        let iteration = 0;
+
+        clearInterval(interval);
+
+        interval = setInterval(()=> {
+            event.innerHTML = event.innerHTML
+                .split("")
+                .map((letter, index) => {
+                    if (index < iteration)
+                    {
+                        return event.dataset.value[index];
+                    }
+                    return letters[Math.floor(Math.random()*26)]
+
+                })
+                .join("");
+
+            if (iteration >= event.dataset.value.length)
+            {
+                clearInterval(interval);
+            }
+
+            iteration += 1 / 3;
+        }, 10);
+    })
 )
