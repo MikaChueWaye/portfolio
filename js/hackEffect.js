ff52f1fc0bd4ecf462d5d25bc4acd2d6e926dfd2
Mika ChueWaye <mika-lock-yane.chue-waye@etu.umontpellier.fr>
2023-04-11 11:34:08 +0200

    Ajout de contenu dans projets et réarrangement de la anv

--- js/hackEffect.js
+++ js/hackEffect.js
@@ -78,3 +78,42 @@ function loadPage()
         }, 10);
     })
 }
+
+// let elements3 = document.querySelectorAll('.fasterHacked');
+//
+// function loadPage()
+// {
+//     elements3.forEach(event => {
+//         let interval = null;
+//
+//         let iteration = 0;
+//
+//         clearInterval(interval);
+//
+//         if (event.dataset.value === undefined)
+//         {
+//             event.dataset.value = event.innerHTML;
+//         }
+//
+//         interval = setInterval(()=> {
+//             event.innerHTML = event.innerHTML
+//                 .split("")
+//                 .map((letter, index) => {
+//                     if (index < iteration)
+//                     {
+//                         return event.dataset.value[index];
+//                     }
+//                     return letters[Math.floor(Math.random()*26)]
+//
+//                 })
+//                 .join("");
+//
+//             if (iteration >= event.dataset.value.length)
+//             {
+//                 clearInterval(interval);
+//             }
+//
+//             iteration += 1;
+//         }, 1);
+//     })
+// }
